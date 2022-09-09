e9eaa194aa5ddd6b1d981b1df2a9119408992b7e
Juseong Hong <jshong9508@gmail.com>
2022-09-09 20:18:10 +0900

    4.4 TO DO LIST_Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+   localStorage.setItem("ToDos", JSON.stringify(toDos)); // Javascript의 어떤 코드건 상관없이 string으로 만들어준다
+}
+
 function deleteToDo(event) {
    const li = event.target.parentElement;
    li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(event) {
    event.preventDefault();
    const newTodo = toDoInput.value;
    toDoInput.value = "";
+   toDos.push(newTodo);
    paintToDo(newTodo);
+   saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
