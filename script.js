9d589cd3749086b3e2cab8b5e0dae9b4f38fc14d
Juseong Hong <jshong9508@gmail.com>
2022-09-09 19:47:07 +0900

    1.2.1 LOGIN_Form Submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
+   const username = loginInput.value;
    console.log("Hello ", loginInput.value);
+   if (username === "") {
+      alert("please write your name.");
+   } else if (username.length > 15) {
+      alert("Your name is too long");
+   } else {
+      alert("Welcome");
+   }
+   // JS로도 작업할 수도있지만 HTML form의 기능을 활용하는 것이 좋다. ex) required, maxlength 등...
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
