290d81357e450b9f49a12c2eded9117755ab8e8f
Juseong Hong <jshong9508@gmail.com>
2022-09-09 20:04:53 +0900

    2.2 CLOCK_Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-   console.log("Hello");
+function getClock() {
+   const date = new Date();
+   //    console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+   clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
-setInterval(sayHello, 1000);
+getClock(); //사이트가 로딩되는 즉시 시간을 바로 보여주고 그 후 1초마다 계속 호출
+setInterval(getClock, 1000);
