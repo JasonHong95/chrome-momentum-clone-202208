57421249d06c798c6c992366757ace25469c9e3a
Juseong Hong <jshong9508@gmail.com>
2022-09-09 19:49:29 +0900

    1.2.2 LOGIN_Form Submission

--- script.js
+++ script.js
@@ -3,15 +3,10 @@ const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
    const username = loginInput.value;
-   console.log("Hello ", loginInput.value);
-   if (username === "") {
-      alert("please write your name.");
-   } else if (username.length > 15) {
-      alert("Your name is too long");
-   } else {
-      alert("Welcome");
-   }
-   // JS로도 작업할 수도있지만 HTML form의 기능을 활용하는 것이 좋다. ex) required, maxlength 등...
+   console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
+
+// Form태그 안에 있는 Button을 누르거나 type이 submit인 input을 클릭하면 자동으로 submit을 하기때문에
+// 브라우저가 새로고침하지 않고 User 정보를 저장하기 위해서는 JS로 submit 되는 걸 막아줘야 한다.
