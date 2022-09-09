d3b53a14c0209ec44df58324294697fe9781b6a9
Juseong Hong <jshong9508@gmail.com>
2022-09-09 19:57:48 +0900

    1.5 LOGIN_Saving Username

--- script.js
+++ script.js
@@ -3,16 +3,13 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-// 반복되는 string들을 대문자 변수로 저장하면 오류를 줄일 수 있다.
-// string은 오타가 나도 오류 검출이 안되나 변수명이 오타가 나면 에러가 뜨기 때문에 문제점을 알기 쉽다.
 
 function onLoginSubmit(event) {
    event.preventDefault();
-   loginForm.classList.add(HIDDEN_CLASSNAME); // HTML form에 "hidden" class 추가
+   loginForm.classList.add(HIDDEN_CLASSNAME);
    const username = loginInput.value;
-   //    greeting.innerText = "Hello " + username; // string과 변수를 합치는 입력방법 1
-
-   greeting.innerText = `Hello ${username}`; // string과 변수를 합치는 입력방법 2
+   localStorage.setItem("username", username); // Local Storage를 이용해서 값을 저장
+   greeting.innerText = `Hello ${username}`;
    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
