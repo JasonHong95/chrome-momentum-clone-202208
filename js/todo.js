ebe7a27dd6616fda941460890dbfcdf08beb60eb
Juseong Hong <jshong9508@gmail.com>
2022-09-09 20:14:46 +0900

    4.2 TO DO LIST_Adding To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+   const li = document.createElement("li");
+   const span = document.createElement("span");
+   li.appendChild(span);
+   span.innerText = newTodo;
+   toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
    event.preventDefault();
    const newTodo = toDoInput.value; // Input의 현재 Value를 새로운변수(newTodo)에 복사
    toDoInput.value = ""; // 이 다음부터 무슨 짓을 하던 newTodo와 상관없음
+   paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
