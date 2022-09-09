b22e20f6bc683741ef9819766a393f1c4ee04a48
Juseong Hong <jshong9508@gmail.com>
2022-09-09 19:51:14 +0900

    1.3 LOGIN_Events

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-   const username = loginInput.value;
-   console.log(username);
+function onLoginSubmit(event) {
+   // 기본값 매개변수
+   event.preventDefault(); // 브라우저의 기본 동작을 실행하지 않도록 설정
+   console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
-
-// Form태그 안에 있는 Button을 누르거나 type이 submit인 input을 클릭하면 자동으로 submit을 하기때문에
-// 브라우저가 새로고침하지 않고 User 정보를 저장하기 위해서는 JS로 submit 되는 걸 막아줘야 한다.
+loginForm.addEventListener("submit", onLoginSubmit);
+//submit event 감지
